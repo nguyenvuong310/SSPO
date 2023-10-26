5b64cc3f1842b468057606b52eef1901864c1ff6
DoanMinhHieu18 <107565744+DoanMinhHieu18@users.noreply.github.com>
2023-10-26 23:45:24 +0700

    Delete client directory

--- client/src/utils/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./constant";
-export { default as CommonUtils } from "./CommonUtils";
-export { default as KeyCodeUtils } from "./KeyCodeUtils";
-
-export { default as ToastUtil } from "./ToastUtil";
